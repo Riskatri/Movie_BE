0ffee48231677212d1ccd47cefdd4fce93d73fa8
Riska <handayaniriska762@gmail.com>
2020-04-02 18:52:00 +0700

    create movie

---
+++ model/movie.js
@@ -0,0 +1,11 @@
+module.exports = (sequelize, Sequelize) => {
+  const Movie = sequelize.define("movies", {
+    original_title: {
+      type: Sequelize.TEXT
+    },
+    release_date: {
+      type: Sequelize.DATE
+    }
+  });
+  return Comment;
+};
